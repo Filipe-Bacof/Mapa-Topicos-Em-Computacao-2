9351dcebae78e7f8f06046a9e0ab15c5080d79ba
Filipe Bacof <filipebacof@gmail.com>
2024-06-25 22:32:00 -0300

    metodo para exibir mensagem

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
 public class HelloWorld {
+
   public static void main(String[] args) {
-    System.out.println("Olá, Mundo!");
-    System.out.println("Este é o meu MAPA de:");
-    System.out.println("Tópicos em Computação II");
+    printMessage("Olá, Mundo!");
+    printMessage("Este é o meu MAPA de:");
+    printMessage("Tópicos em Computação II");
+  }
+
+  public static void printMessage(String message) {
+    System.out.println(message);
   }
 }
